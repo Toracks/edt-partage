3f84b71bcfbdb78da53b6d6eae836ecbe8c87d90
Toracks <charlestomlm@gmail.com>
2026-05-24 14:42:11 +0200

    fix render structure

--- Backend/server.py
+++ Backend/server.py
@@ -1,16 +1,22 @@
-from flask import Flask, send_from_directory
 import os
+from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_folder="../frontend")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+FRONTEND_DIR = os.path.join(BASE_DIR, "..", "frontend")
 
+app = Flask(__name__)
+
+# Page principale
 @app.route("/")
 def home():
-    return send_from_directory(app.static_folder, "index.html")
+    return send_from_directory(FRONTEND_DIR, "index.html")
 
+# Fichiers statiques (css, js, images)
 @app.route("/<path:path>")
 def static_files(path):
-    return send_from_directory(app.static_folder, path)
+    return send_from_directory(FRONTEND_DIR, path)
 
+# Lancement serveur (important pour Render)
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
