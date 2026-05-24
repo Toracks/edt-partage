72dc523abb8283bfac847aba44d966a37499fcd4
Toracks <charlestomlm@gmail.com>
2026-05-24 15:04:58 +0200

    prepare auth system

--- start.bat
+++ start.bat
@@ -1,14 +1,4 @@
 @echo off
-cd /d %~dp0
-
-echo Activation environnement virtuel...
-
-call venv\Scripts\activate.bat
-
-echo Lancement du serveur Flask...
-
-start cmd /k "python backend/server.py"
-
-timeout /t 2 >nul
-
-start http://127.0.0.1:5001
+echo Ouverture du site...
+start "" https://edt-partage.onrender.com
+exit
